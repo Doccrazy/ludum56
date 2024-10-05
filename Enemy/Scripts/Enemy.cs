4890d15151fb312e1ca4cc46491f62895440020e
Nico Eberl <n.eberl@tiffinger-thiel.de>
2024-10-05 15:32:09 +0200

    Create enemy (wip)

--- Enemy/Scripts/Enemy.cs
+++ Enemy/Scripts/Enemy.cs
@@ -3,14 +3,31 @@ using System;
 
 public partial class Enemy : Node
 {
-	// Called when the node enters the scene tree for the first time.
+	[Export]
+	protected bool isActive = false;
+
+	[Export]
+	protected int speed = 10;
+
+	[Export]
+	protected bool isDead = false;
+
+	[Export]
+	protected float damage = 10f;
+
+	[Export]
+	protected CharacterBody3D character;
+
+
+	// Called when the node enters the scene tree for the first time.p
 	public override void _Ready()
 	{
-		
+
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+
 	}
 }
