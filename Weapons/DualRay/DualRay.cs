c74eacf093e47f050272619976f309b2ab34d06f
Matthias Piepkorn <m.piepkorn@tiffinger-thiel.de>
2024-10-06 10:23:31 +0200

    add dual ray weapon

---
+++ Weapons/DualRay/DualRay.cs
@@ -0,0 +1,24 @@
+using Godot;
+using System;
+
+public partial class DualRay : Node3D, IWeapon
+{
+	[Export]
+	public Node[] Emitters;
+
+	public bool Emitting { get; set; }
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		foreach (var emitter in Emitters)
+		{
+			(emitter as IWeapon).Emitting = Emitting;
+		}
+	}
+}
